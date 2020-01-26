01ee6b635d334e81b7ede80b4317759539da1eea
Mulk Abdulhadi <mabdulul@gmail.com>
2020-01-26 02:00:14 -0500

    adding textconnection

--- src/components/Quiz_componets/hairroutine.jsx
+++ src/components/Quiz_componets/hairroutine.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 //StyleSheet
@@ -6,10 +6,33 @@ import "../../Stylesheets/Quiz/quizStyles.css";
 
 const HairRoutine = ({ history }) => {
 	const { register, handleSubmit } = useForm();
-	const onSubmit = (data, e) => {
+	const [errorMsg, setMsg] = useState("");
+	const onSubmit = async (data, e) => {
 		e.preventDefault();
 		console.log(data);
-		history.push("/hairroutine");
+		const hairroutine = data.hairroutine;
+		console.log(hairroutine);
+
+		if (!hairroutine) {
+			setMsg(
+				"Please just give us a little bit info. It would really help us out"
+			);
+			// console.log("Please try again");
+		} else {
+			const response = await fetch(
+				"http://localhost:3080/hair/form/hairroutine",
+				{
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(data)
+				}
+			);
+			console.log(response);
+			history.push("/hairroutine");
+		}
 	};
 	const BacktoHairColor = () => {
 		history.push("/coloredhair");
@@ -34,12 +57,13 @@ const HairRoutine = ({ history }) => {
 							<span className="QuizForm_end ">END</span>
 						</div>
 						<div className="QuizForm-header">
+							<p className="errorMsg">{errorMsg}</p>
 							<h4>How do you style your hair ?</h4>
 						</div>
 						<div className="QuizForm__label">
 							<textarea
 								className="hairRoutine-textbox"
-								name="hairRoutine"
+								name="hairroutine"
 								ref={register}
 							></textarea>
 						</div>
