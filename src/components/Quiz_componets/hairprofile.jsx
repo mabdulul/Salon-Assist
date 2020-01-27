b894e9ec69e9eee5434c44b02f20d1fa618df44b
Mulk Abdulhadi <mabdulul@gmail.com>
2020-01-27 18:17:40 -0500

    just adding values from the api

--- src/components/Quiz_componets/hairprofile.jsx
+++ src/components/Quiz_componets/hairprofile.jsx
@@ -5,8 +5,18 @@ import "../../Stylesheets/Quiz/quizStyles.css";
 
 const HairProfile = () => {
 	let user_id = localStorage.personid;
+	const [loading, setLoading] = useState(true);
+	const [firstName, setfirstName] = useState("");
+	const [hairtype, setHair] = useState("");
+	const [length, setlength] = useState("");
+	const [typeofColor, settypeofColor] = useState("");
+	const [hairstructure, setshairstructure] = useState("");
+	const [hairColor, sethairColor] = useState("");
+	const [hairRoutine, setHairRoutine] = useState("");
 
 	useEffect(() => {
+		let user_id = localStorage.personid;
+		setfirstName(localStorage.firstname);
 		getData();
 		async function getData() {
 			const response = await fetch(
@@ -14,6 +24,13 @@ const HairProfile = () => {
 			);
 
 			const data = await response.json();
+			setHair(data.hairtype);
+			setlength(data.length_hair);
+			settypeofColor(data.boxdye_salon);
+			setshairstructure(data.hair_structure);
+			sethairColor(data.haircolor);
+			setHairRoutine(data.hairroutine);
+			setLoading(false);
 			console.log("here", data);
 		}
 	}, [user_id]);
@@ -21,7 +38,28 @@ const HairProfile = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<h1>Hello There</h1>
+				{loading ? (
+					<div>...loading</div>
+				) : (
+					<>
+						<h1>Hi , {firstName}</h1>
+
+						<ul>
+							<li>{hairtype}</li>
+							<li>{length}</li>
+							<li>
+								{typeofColor === "Salon" ? (
+									<>recently dye at salon</>
+								) : (
+									<>recently colored using box dye</>
+								)}
+							</li>
+							<li>{hairstructure}</li>
+							<li>You currently have {hairColor}</li>
+							<li>{hairRoutine}</li>
+						</ul>
+					</>
+				)}
 			</div>
 		</div>
 	);
