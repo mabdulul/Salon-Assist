6b88f70e3dc004aafbdec08ae51b570b5ce61504
Mulk Abdulhadi <mabdulul@gmail.com>
2020-02-03 08:45:50 -0500

    added a booking appt

--- src/components/Makeappt/bookingone.jsx
+++ src/components/Makeappt/bookingone.jsx
@@ -3,16 +3,20 @@ import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 import { useForm } from "react-hook-form";
+import moment from "moment";
 
 const BookingOne = () => {
 	const { register, handleSubmit } = useForm();
 	const [errorMsg, setMsg] = useState("");
 	const [startDate, setStartDate] = useState(new Date());
-	console.log(startDate);
+
 	const onSubmit = async (data, e) => {
 		e.preventDefault();
 		console.log(data);
-		console.log("here", startDate);
+
+		var day = moment(startDate);
+		var t = day.format("YYYY-MM-DD HH:mm:ss");
+		console.log("I am t", t);
 	};
 	return (
 		<div className="container">
@@ -26,10 +30,33 @@ const BookingOne = () => {
 							<p className="errorMsg">{errorMsg}</p>
 							<h4>REQUEST A NEW APPOINTMENT</h4>
 						</div>
+
 						<DatePicker
 							selected={startDate}
 							onChange={date => setStartDate(date)}
+							showTimeSelect
+							timeFormat="HH:mm"
+							timeIntervals={15}
+							timeCaption="time"
+							dateFormat="MMMM d, yyyy h:mm aa"
+							ref={register}
+							name="date"
 						/>
+						<label>
+							<select ref={register} name="service">
+								<option value="Balayage/Ombre">Balayage/Ombre</option>
+								<option value="Master Designer">Master Designer</option>
+								<option value="Shampoo/BlowDry">Shampoo/Blow Dry</option>
+								<option value="Keratin Treatment">KeratinTreatment</option>
+							</select>
+						</label>
+						<label>
+							<select ref={register} name="stylist">
+								<option value="Rahma Inman">Rahma Inman</option>
+								<option value="Sarah Hill">Sarah Hill</option>
+								<option value="Jess Garcia">Jess Garcia</option>
+							</select>
+						</label>
 					</div>
 					<div className="QuizForm__btnBlock col-sm-12 col-md-12 col-lg-12">
 						<button className="QuizForm__btn" type="submit">
